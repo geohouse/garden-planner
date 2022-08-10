5e53d4c7f304669aa8dd4b98cb255e1701b0ae6e
Geoffrey House <geo.house7@gmail.com>
2022-08-10 17:37:09 -0600

    Added React app framework

--- dist/index.579125c3.js
+++
@@ -1,7 +0,0 @@
-const mainLabel = document.querySelector(".main-label");
-mainLabel.addEventListener("click", ()=>{
-    if (mainLabel.innerText === "Garden planner") mainLabel.innerText = "Still being planted";
-    else if (mainLabel.innerText === "Still being planted") mainLabel.innerText = "Garden planner";
-});
-
-//# sourceMappingURL=index.579125c3.js.map
