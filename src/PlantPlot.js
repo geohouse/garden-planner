2a329fb5e020e4c7df0f4af9f9d3d8e596763f51
Geoffrey House <geo.house7@gmail.com>
2022-08-15 22:40:27 -0600

    Fully functional basic PlantPlot from bloom month

--- src/PlantPlot.js
+++ src/PlantPlot.js
@@ -39,15 +39,35 @@ export default function PlantPlot(props) {
     "Oct",
     "Nov",
     "Dec",
+    "Jan",
   ];
 
   function createDatasets() {
     const numPlants = inputPlants.length;
+    // Initialize a holder array with undefined values (any surviving
+    // undefined entries just appear as holes in the graph)
+    const templateBloomArray = Array.from({ length: 12 });
     console.log({ numPlants });
-    const datasetsArray = inputPlants.map((plant, index) => {
+    const datasetsArray = inputPlants.map((plant, plantIndex) => {
+      let bloomArray = [...templateBloomArray];
+      // Only prep the graph data if there was at least 1 bloom time noted.
+      // Otherwise the array to plot will all be undefined values, and nothing will
+      // show on the graph.
+      if (plant.bloomTime.monthNumAsStringArray.length > 0) {
+        console.log("Firing if");
+        plant.bloomTime.monthNumAsStringArray.forEach((bloomTimeEntryStr) => {
+          const bloomTimeEntryNum = Number.parseInt(bloomTimeEntryStr, 10);
+          // Fill in the graph entry to span the bloom month start and the bloom month end
+          // Graph indices are 0-based
+          bloomArray[bloomTimeEntryNum - 1] = plantIndex + 1;
+          bloomArray[bloomTimeEntryNum] = plantIndex + 1;
+          console.log({ bloomArray });
+        });
+        console.log(bloomArray);
+      }
       const currPlantForGraph = {
         label: plant.plantName,
-        data: [index, index, index],
+        data: bloomArray,
         borderColor: plant.bloomColor,
         backgroundColor: plant.bloomColor,
       };
