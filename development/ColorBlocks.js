ccc6c1ee32b2df4fcb77a9647c7387f4b4e08330
Geoffrey House <geo.house7@gmail.com>
2022-08-18 18:46:30 -0600

    start building color block selector

---
+++ development/ColorBlocks.js
@@ -0,0 +1,18 @@
+export default function ColorBlocks() {
+  return <>Testing color blocks</>;
+}
+
+const colors = {
+  red: "",
+  orange: "",
+  yellow: "",
+  green: "",
+  blue: "",
+  purple: "",
+  white: "",
+  pink: "",
+  lightBlue: "",
+  lavender: "",
+  lightOrange: "",
+  lightYellow: "",
+};
