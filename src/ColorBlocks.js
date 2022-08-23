c6297eab8379ab23234caa254f2cabec4c87991f
Geoffrey House <geo.house7@gmail.com>
2022-08-22 22:21:09 -0600

    Working color selector component
    Need to still integrate state with the main app

--- src/ColorBlocks.js
+++ src/ColorBlocks.js
@@ -1,4 +1,21 @@
+import { useState, useEffect } from "react";
 export default function ColorBlocks() {
+  const [currColor, setCurrentColor] = useState("");
+
+  // Update the selected border every time the color selection is changed
+  useEffect(() => {
+    const allColorButtons = document.querySelectorAll(".color-block-button");
+    allColorButtons.forEach((colorButton) => {
+      if (colorButton.innerHTML === currColor) {
+        colorButton.classList.toggle("selected-color");
+      } else {
+        // Calling remove on the classList is safe even if the 'selected-color'
+        // class isn't in the classList.
+        colorButton.classList.remove("selected-color");
+      }
+    });
+  }, [currColor]);
+
   const colors = {
     Red: "#ff0033",
     Orange: "#ff6600",
@@ -15,22 +32,48 @@ export default function ColorBlocks() {
     None: "#ffffff",
   };
 
+  function handleColorChange(event) {
+    event.preventDefault();
+    console.log("clicked");
+    console.log(event.currentTarget.innerHTML);
+    setCurrentColor(event.currentTarget.innerHTML);
+  }
+
   return (
     <>
       <div id="color-block-holder">
         {Object.values(colors).map((color, index) => {
           const colorName = Object.keys(colors)[index];
           console.log(colorName);
-          const colorStyle = { backgroundColor: { color } };
+          // Only works when don't wrap color in {}
+          const colorStyle = { backgroundColor: color };
           console.log(colorStyle);
+          let classList = "color-block-button";
+          // Set colors to change the text color for better contrast
+          // depending on the bloom color option.
+          if (
+            ["Red", "Orange", "Green", "Blue", "Purple"].includes(colorName)
+          ) {
+            classList += " light-text";
+          } else {
+            classList += " dark-text";
+          }
+
           return (
-            <button
-              key={index}
-              className="color-block-button"
-              style={colorStyle}
-            >
-              {colorName}
-            </button>
+            <>
+              {/* <label id={`label-${colorName}`} for={colorName}>
+                {colorName}
+              </label> */}
+              <button
+                key={index}
+                className={classList}
+                style={colorStyle}
+                onClick={handleColorChange}
+                // id={colorName}
+              >
+                {colorName}
+              </button>
+            </>
           );
         })}
       </div>
