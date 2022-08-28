96dcd028892ed11f8d5893cf5510d8a1d559e60c
Geoffrey House <geo.house7@gmail.com>
2022-08-27 22:42:07 -0600

    working bloom time selector with graphing

--- src/BloomDateSelect.js
+++ src/BloomDateSelect.js
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "react";
 export default function BloomDateSelect(props) {
-  const [selectedMonths, setSelectedMonths] = useState();
+  const [selectedMonths, setSelectedMonths] = useState({});
+
+  useEffect(() => {
+    console.log("The new selected months are:");
+    console.log(selectedMonths);
+  }, [selectedMonths]);
 
   const { onBloomTimeChange } = props;
   const months = {
@@ -24,7 +29,10 @@ export default function BloomDateSelect(props) {
     monthButtonList.forEach((monthButton) => {
       monthButton.classList.add("selected-month");
     });
+    // Use the months object directly to set the state because
+    // all months have been selected.
     onBloomTimeChange(months);
+    setSelectedMonths(months);
   }
 
   function handleNoMonthSelect(event) {
@@ -33,12 +41,54 @@ export default function BloomDateSelect(props) {
     monthButtonList.forEach((monthButton) => {
       monthButton.classList.remove("selected-month");
     });
+    // Use empty object to set the state because none of the months
+    // are selected
+    onBloomTimeChange({});
+    setSelectedMonths({});
+  }
+
+  function updateCurrentlySelectedMonthsObj() {
+    const monthButtonList = document.querySelectorAll(".bloom-month");
+    monthButtonList.forEach((monthButton) => {
+      //This is a DOMTokenList, and uses the .contains() method
+      // (doesn't have an .includes() method)
+      let monthNum = 0;
+      const monthName = monthButton.innerText;
+      for (const keyValueArray of Object.entries(months)) {
+        if (keyValueArray[1] === monthName) {
+          monthNum = keyValueArray[0];
+        }
+      }
+      // ADD any newly selected months if this button is selected
+      // and is not already in the list
+      if (
+        monthButton.classList.contains("selected-month") &&
+        !Object.keys(selectedMonths).includes(monthNum)
+      ) {
+        const newSelectedMonths = { ...selectedMonths };
+        newSelectedMonths[monthNum] = monthName;
+        setSelectedMonths(newSelectedMonths);
+        onBloomTimeChange(newSelectedMonths);
+      }
+      // REMOVE any months from the list that aren't selected any longer.
+      if (
+        Object.keys(selectedMonths).includes(monthNum) &&
+        !monthButton.classList.contains("selected-month")
+      ) {
+        // deconstruct the selectedMonths obj using the dynamic value of monthNum
+        // to extract the part to remove (need to do : assignment for dynamic destructuring to work)
+        const { [monthNum]: toRemove, ...rest } = selectedMonths;
+        setSelectedMonths({ ...rest });
+        onBloomTimeChange({ ...rest });
+      }
+    });
   }
 
   function handleMonthToggle(event) {
     console.log("month toggle");
     console.log(event.currentTarget);
     event.currentTarget.classList.toggle("selected-month");
+    updateCurrentlySelectedMonthsObj();
   }
 
   function handleMonthMouseOver(event) {
@@ -59,7 +109,11 @@ export default function BloomDateSelect(props) {
 
   return (
     <>
-      <button id="select-all-months" onClick={handleAllMonthSelect}>
+      <button
+        id="select-all-months"
+        type="button"
+        onClick={handleAllMonthSelect}
+      >
         Select all months
       </button>
       {Object.values(months).map((monthName, index) => {
@@ -77,7 +131,7 @@ export default function BloomDateSelect(props) {
           </button>
         );
       })}
-      <button id="select-no-months" onClick={handleNoMonthSelect}>
+      <button id="select-no-months" type="button" onClick={handleNoMonthSelect}>
         Select no months
       </button>
     </>
