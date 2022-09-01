5e13b14cb61cca0e016ae2347ffee45924ef2a22
Geoffrey House <geo.house7@gmail.com>
2022-09-01 16:20:25 -0600

    ColorBlocks passing TS

--- src/AddPlant.tsx
+++ src/AddPlant.tsx
@@ -9,6 +9,10 @@ interface BloomTime {
   monthNameArray: string[];
 }
 
+// interfaces are only used for object types. `type` can be used to alias the type
+// for other features e.g. functions.
+//type BloomColorChange = (hexColor: string, colorName: string) => void;
+
 // This accomodates the keys being 1-12 (or any number) without any hardcoding.
 // Could make more specific to only allow 1-12 and that would also be OK.
 interface BloomTimeObj {
