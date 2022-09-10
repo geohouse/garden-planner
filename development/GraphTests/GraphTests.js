b4d1f4528133befe73da519d7c4057d119c3180a
Geoffrey House <geo.house7@gmail.com>
2022-09-10 16:23:56 -0600

    Plant plot y tick positions and labels

--- development/GraphTests/GraphTests.js
+++ development/GraphTests/GraphTests.js
@@ -60,6 +60,25 @@ const testChart = new Chart(ctx, {
         borderColor: "#aa0099",
         backgroundColor: "#aa0099",
       },
+      {
+        label: "Marigold_other",
+        data: [
+          undefined,
+          0.6,
+          0.6,
+          0.6,
+          0.6,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+        ],
+        borderColor: "#44aadd",
+        backgroundColor: "#aaff99",
+      },
     ],
   },
   options: {
@@ -67,6 +86,47 @@ const testChart = new Chart(ctx, {
       y: {
         min: 0,
         max: 5,
+        afterBuildTicks: axis => axis.ticks = [0.6,0.8,1.0].map(entry => ({value: entry})),
+        ticks: {
+
+          callback: function (value, index, ticks) {
+            // ticks is an array of objects with each object having 'value', 'label', and '$context' keys.
+            // 'value' is the tick mark value on the axis, and 'label' is the current tick label
+            // index is the object number in the ticks array that represents the current value.
+            let testType= [
+              { bloom: 1.0, fruit: 0.8, other: 0.6 },
+            ];
+            return value === 1 ? 'bloom' : value === 0.8 ? 'fruit' : 'other'
+
+            // console.log({ value });
+            // console.log({ ticks });
+            // console.log({ index });
+            // for (let typeObj of testType) {
+            //   // bloom, fruit, or other
+            //   for (let typeKey in typeObj) {
+            //     let typeValue = typeObj[typeKey];
+            //     console.log({ typeValue });
+            //     for (let currTickObject of ticks) {
+            //       console.log(currTickObject);
+            //       console.log(currTickObject.value === typeValue);
+            //       if (currTickObject.value === typeValue) {
+            //         currTickObject.label = typeKey;
+            //         //return typeKey;
+            //       }
+            //     }
+            //   }
+            // }
+
+            // if (index === 3) {
+            //   return `${ticks[index].value} blooming test`;
+            // }
+
+            // return `*${value}`;
+          },
+          // callback: function (value, index) {
+          //   return `# ${value}`;
+          // },
+        },
       },
     },
   },
--- src/PlantPlot.tsx
+++ src/PlantPlot.tsx
@@ -9,6 +9,8 @@ import {
   Legend,
 } from "chart.js";
 
+import type { Tick } from "chart.js";
+
 import { Line } from "react-chartjs-2";
 
 import { PlantsType } from "./GardenPlannerInterfaces";
@@ -86,6 +88,18 @@ export default function PlantPlot(props: PlantPlotProps) {
     return plantCharacteristicsToPlot;
   }
 
+  // will be used to create the correct y axis tick marks and labels
+  let yTickArray: { bloom: number; fruit: number; other: number }[] = [];
+
+  function createYTickObj_singlePlant(plantIndex: number) {
+    const holderObj = {
+      bloom: plantIndex + 1,
+      fruit: plantIndex + 0.9,
+      other: plantIndex + 0.8,
+    };
+    yTickArray.push(holderObj);
+  }
+
   function createSinglePlantData(
     inputPlantObj: PlantsType,
     plantIndex: number
@@ -98,6 +112,8 @@ export default function PlantPlot(props: PlantPlotProps) {
       plantCharIndex < plantCharacteristicsToPlot.length;
       plantCharIndex++
     ) {
+      // Pass the index to make the ytick label data structure
+      createYTickObj_singlePlant(plantIndex);
       let plantEvent = plantCharacteristicsToPlot[plantCharIndex];
       // Keep going if this was the initial placeholder empty string in the array
       if (plantEvent === "") {
@@ -148,6 +164,7 @@ export default function PlantPlot(props: PlantPlotProps) {
 
   function createDatasets() {
     let datasetArray = [];
+
     for (let plantIndex = 0; plantIndex < inputPlants.length; plantIndex++) {
       let currPlant = inputPlants[plantIndex];
       let currPlantDataArray = createSinglePlantData(currPlant, plantIndex);
@@ -166,20 +183,69 @@ export default function PlantPlot(props: PlantPlotProps) {
     datasets: plottingData,
   };
 
-  const plottingOptions = {
+  let bloomArray: number[] = [];
+  let fruitArray: number[] = [];
+  let otherArray: number[] = [];
+
+  for (let typeObj of yTickArray) {
+    bloomArray.push(typeObj.bloom);
+    fruitArray.push(typeObj.fruit);
+    otherArray.push(typeObj.other);
+  }
+  // combined array needed in order to filter the ticks to just include those represented in this array
+  const combinedArray = bloomArray.concat(fruitArray).concat(otherArray);
+
+  // I was getting lots of type errors when setting the ticks callback - it's supposed to be a fancy
+  // intersection type combining lots of different subtypes for the Line component, but was very hard
+  // for me to understand what was missing and needed to be typed differently, so I'm making plottingOptions
+  // an 'any' type to avoid the type errors and to continue on. I realize this isn't the best,
+  // but it's the only way I could clear this error for now.
+  const plottingOptions: any = {
     scales: {
       y: {
+        // Filter the y axis ticks to only include those that are represented by a plant event (bloom, fruit, other)
+        // and ensure that those are created. I don't know the type of the input axis parameter to the afterBuildTicks
+        // function, so set to any.
+        afterBuildTicks: function (axis: any): void {
+          axis.ticks = combinedArray.map((entry) => ({ value: entry }));
+        },
         ticks: {
-          color: "#ff0000",
-          // callback: function(val, index) {
-          //   // Hide every 2nd tick label
-          //   return index % 2 === 0 ? this.getLabelForValue(val) : '';
-          // },
+          //color: "#ff0000",
+
+          // types for callback function
+          // The Tick[] type is found from the types definition file (https://github.com/chartjs/Chart.js/blob/master/types/index.d.ts)
+          // and allows for correct typing/autocompletion of property names below.
+          // Interface for Tick from the .d.ts file:
+          // export interface Tick {
+          //   value: number;
+          //   label?: string | string[];
+          //   major?: boolean;
+          // }
+
+          // This is the function that sets the labels on the y axis ticks
+          callback: function (
+            value: number,
+            index: number,
+            ticks: Tick[]
+          ): string | undefined {
+            // ticks is an array of objects with each object having 'value', 'label', and '$context' keys.
+            // 'value' is the tick mark value on the axis, and 'label' is the current tick label
+            // index is the object number in the ticks array that represents the current value.
+
+            // Determine the correct tick label to return
+            return bloomArray.includes(value)
+              ? "Bloom"
+              : fruitArray.includes(value)
+              ? "Fruit"
+              : otherArray.includes(value)
+              ? "Other"
+              : undefined;
+          },
         },
       },
       x: {
         ticks: {
-          color: "#00ffff",
+          //color: "#00ffff",
         },
       },
     },
